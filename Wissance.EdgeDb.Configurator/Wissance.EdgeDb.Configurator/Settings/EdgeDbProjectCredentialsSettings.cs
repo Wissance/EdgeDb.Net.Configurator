3c3f94411ede75da519f24cf6c9875fcb9a92a5f
Ushakov Michael <um.nix.user@gmail.com>
2023-08-29 22:47:52 +0500

    full properties specification && reduced version of lib

--- Wissance.EdgeDb.Configurator/Wissance.EdgeDb.Configurator/Settings/EdgeDbProjectCredentialsSettings.cs
+++ Wissance.EdgeDb.Configurator/Wissance.EdgeDb.Configurator/Settings/EdgeDbProjectCredentialsSettings.cs
@@ -4,9 +4,13 @@ namespace Wissance.EdgeDb.Configurator.Settings
 {
     public class EdgeDbProjectCredentialsSettings
     {
+        [JsonPropertyName("port")]
         public int Port { get; set; }
+        [JsonPropertyName("user")]
         public string User { get; set; }
+        [JsonPropertyName("password")]
         public string Password { get; set; }
+        [JsonPropertyName("database")]
         public string Database { get; set; }
         [JsonPropertyName("tls_cert_data")]
         public string TlsCertData { get; set; }
